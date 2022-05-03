5d0d94758533da6cb254253fb02c87b8954b01dd
rohit <rohit9702871521@gmail.com>
2022-05-03 19:41:29 +0530

    sidebar done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider, moralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return ( <
+        MoralisProvider serverUrl = { process.env.NEXT_PUBLIC_MORALIS_SERVER }
+        appId = { process.env.NEXT_PUBLIC_MORALIS_APP_ID } >
+        <
+        Component {...pageProps }
+        /> < /
+        MoralisProvider >
+    );
 }
 
-export default MyApp
+export default MyApp;
